3f2ba44763ebe6226f6e866e42e8b9fc3bdf636d
ankur6ue <ankur6ue@gmail.com>
2019-10-23 16:46:30 -0400

    first version of face recognition system. Only detection implemented so far

---
+++ client/faceRec.js
@@ -0,0 +1,159 @@
+/**
+ * Client side of PyTorch Detection Web API
+ * Initial version taken from webrtcHacks - https://webrtchacks.com
+ */
+
+
+
+//Parameters
+const s = document.getElementById('faceRec');
+const sourceVideo = s.getAttribute("data-source");  //the source video to use
+const uploadWidth = s.getAttribute("data-uploadWidth") || 640; //the width of the upload file
+const mirror = s.getAttribute("data-mirror") || false; //mirror the boundary boxes
+const scoreThreshold = s.getAttribute("data-scoreThreshold") || 0.5;
+const hostType = s.getAttribute("data-apiServer")
+const ovWidth = 320
+const ovHeight = 240
+var procType = "gpu"
+
+if (hostType == 'localhost')
+	apiServer = "http://127.0.0.1:5000" // must be just like this. using 0.0.0.0 for the IP doesn't work! 
+else
+	apiServer = "https://www.telesens.co/obj_detect_impl"
+
+//Video element selector
+v = document.getElementById(sourceVideo);
+
+//for starting events
+let isPlaying = false,
+    gotMetadata = false;
+
+//Canvas setup
+
+//create a canvas to grab an image for upload
+let imageCanvas = document.createElement('canvas');
+let imageCtx = imageCanvas.getContext("2d");
+
+//create a canvas for drawing object boundaries
+let drawCanvas = document.createElement('canvas');
+document.body.appendChild(drawCanvas);
+let drawCtx = drawCanvas.getContext("2d");
+
+//draw boxes and labels on each detected object
+function drawBoxes(objects) {
+
+    //clear the previous drawings
+    drawCtx.clearRect(0, 0, drawCanvas.width, drawCanvas.height);
+
+    //filter out objects that contain a class_name and then draw boxes and labels on each
+    objects.filter(object => object.class_name).forEach(object => {
+
+        let x = 2*object.x;
+        let y = 2*object.y;
+        let width = 2*(object.width);
+        let height = 2*(object.height);
+
+        //flip the x axis if local video is mirrored
+        if (mirror) {
+            x = drawCanvas.width - (x + width)
+        }
+
+        drawCtx.fillText(object.class_name + " - " + Math.round(object.score * 100) + "%", x + 5, y + 20);
+        drawCtx.strokeRect(x, y, width, height);
+
+    });
+}
+
+//Add file blob to a form and post
+function postFile(file) {
+
+    //Set options as form data
+    let formdata = new FormData();
+    formdata.append("image", file);
+    formdata.append("threshold", scoreThreshold);
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', apiServer + '/detect/' + procType, true);
+    xhr.onload = function () {
+        if (this.status === 200) {
+            let object_data = JSON.parse(this.response);
+
+            //draw the boxes
+            drawBoxes(object_data.objects);
+
+            //Save and send the next image
+            imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight, 0, 0, ovWidth, ovHeight );
+            imageCanvas.toBlob(postFile, 'image/jpeg');
+        }
+        else {
+            console.error(xhr);
+        }
+    };
+    xhr.send(formdata);
+	//xhr.send();
+}
+
+//Start object detection
+function startObjectDetection() {
+
+    console.log("starting Face Recognition");
+    
+    //Set canvas sizes base don input video
+    drawCanvas.width = v.videoWidth;
+    drawCanvas.height = v.videoHeight;
+
+    imageCanvas.width = ovWidth;
+    imageCanvas.height = ovHeight;
+
+    //Some styles for the drawcanvas
+    drawCtx.lineWidth = 4;
+    drawCtx.strokeStyle = "cyan";
+    drawCtx.font = "20px Verdana";
+    drawCtx.fillStyle = "cyan";
+
+    //Save and send the first image
+    imageCtx.drawImage(v, 0, 0, v.videoWidth, v.videoHeight, 0, 0, ovWidth, ovHeight);
+    let xhr = new XMLHttpRequest();
+	var method = apiServer + '/init'
+    xhr.open('GET', method, true);
+	xhr.onload = function () {
+        if (this.status === 200) {
+			imageCanvas.toBlob(postFile, 'image/jpeg');
+		}
+	};
+	xhr.send()
+
+}
+
+//Starting events
+
+//check if metadata is ready - we need the video size
+v.onloadedmetadata = () => {
+    console.log("video metadata ready");
+    gotMetadata = true;
+    if (isPlaying)
+	{
+        startObjectDetection();
+	}
+};
+
+//see if the video has started playing
+v.onplaying = () => {
+    console.log("video playing");
+    isPlaying = true;
+    if (gotMetadata) {
+        startObjectDetection();
+    }
+};
+
+/*
+
+document.addEventListener("DOMContentLoaded", function() {
+	$('#ProcList').append($('<option>', {value:"cpu", text:"CPU"}));
+	$('#ProcList').append($('<option>', {value:"gpu", text:"GPU"}));
+}, false);
+
+$('#ProcList').change(function(){ 
+    procType = $(this).val();
+	
+});
+*/

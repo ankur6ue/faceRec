3b578b8d5ff4c345b45c438597d6453c6573ebd7
ankur6ue <ankur6ue@gmail.com>
2019-10-24 11:30:19 -0400

    fixed shallow copy bug in config

--- server/config.py
+++ server/config.py
@@ -2,12 +2,12 @@ cfg = {
     'faceDetThreshold': '0.7'
 }
 
-ubuntu_cfg = cfg
+ubuntu_cfg = cfg.copy()
 ubuntu_cfg['log_file'] = '/home/dev/faceRec/server/mesg.log'
 ubuntu_cfg['ssl_crt'] = 'ssl/faceRec.crt'
 ubuntu_cfg['ssl_key'] = 'ssl/faceRec.key'
 
-win_cfg = cfg
+win_cfg = cfg.copy()
 
 win_cfg['log_file'] = 'C:\\Telesens\\web\\faceRec\\client\\mesg.log'
 win_cfg['ssl_crt'] = ''

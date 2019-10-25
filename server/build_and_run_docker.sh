cc233c233c40bdd9a21abbc501dc7fc6a93d0f4d
ankur6ue <ankur6ue@gmail.com>
2019-10-25 09:46:02 -0400

    Updated readme for v0.1, added run in background (-d) for the docker container run script

--- server/build_and_run_docker.sh
+++ server/build_and_run_docker.sh
@@ -2,4 +2,4 @@
 app="docker.face_det"
 docker image build -t ${app} .
 docker system prune
-docker container run -p 5000:5000 --name face_det ${app}
+docker container run -d -p 5000:5000 --name face_det ${app}

1ca51f37762f72b11432d915ba4803393f31a6ed
Ankur <ankur6ue@gmail.com>
2019-10-26 14:44:16 +0000

    updated Dockerfiles

--- server/build_and_run_docker.sh
+++ server/build_and_run_docker.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 app="docker.face_det"
-docker image build -t ${app} .
-docker system prune
-docker container run -p 5000:5000 --name face_det ${app}
+version="0.1"
+#-rm removes intermediate containers
+docker image build --rm --tag ${app} .
+#docker system prune
+#docker container run -t -i -d -p 5000:5000 ${app}
+
+# push package to github
+# login
+docker login docker.pkg.github.com -u ankur6ue -p 2885fdef1353250626580e5c9bc2c8826a07e3ce
+# tag
+docker tag ${app} docker.pkg.github.com/ankur6ue/facerec/${app}:${version}
+# push
+docker push docker.pkg.github.com/ankur6ue/facerec/${app}:${version}

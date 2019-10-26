0cfa1689fa6a53b5b66f8372b85be34430affa9d
Ankur <ankur6ue@gmail.com>
2019-10-26 17:32:37 +0000

    added functionality to read github token from file

--- server/build_and_run_docker.sh
+++ server/build_and_run_docker.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 app="docker.face_det"
 version="0.1"
+
+# read github access token from file
+input="github_token.txt"
+token=$(head -n 1 $input)
+echo "$token"
+
 #-rm removes intermediate containers
 docker image build --rm --tag ${app} .
 #docker system prune
 #docker container run -t -i -d -p 5000:5000 ${app}
 
-# push package to github
-# login
-docker login docker.pkg.github.com -u ankur6ue -p 2885fdef1353250626580e5c9bc2c8826a07e3ce
-# tag
-docker tag ${app} docker.pkg.github.com/ankur6ue/facerec/${app}:${version}
-# push
-docker push docker.pkg.github.com/ankur6ue/facerec/${app}:${version}
+# if token exists then try to upload to github
+if [ -n "$token" ]; then
+    # push package to github
+    # login
+    docker login docker.pkg.github.com -u ankur6ue -p $token
+    # tag
+    docker tag ${app} docker.pkg.github.com/ankur6ue/facerec/${app}:${version}
+    # push
+    docker push docker.pkg.github.com/ankur6ue/facerec/${app}:${version}
+fi
